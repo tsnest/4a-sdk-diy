19f66ccd035d88f3201f675e63ca0044f90f1279
Modera <cat-modera@yandex.ru>
2021-11-01 13:39:51 +0300

    Исправленный скрипт js\ll\map_params.js от TheStalkerNest

--- js/ll/map_params.js
+++ js/ll/map_params.js
@@ -4,9 +4,9 @@
 
 // HEADER INFO
 reader.ReadU8('mp_mode');
-reader.ReadString('mp_name');
+reader.ReadString('map_name');
 reader.ReadString('name');
-reader.ReadHintStr('caption', 'chose');
+reader.ReadHintStr('caption', 'choose');
 reader.ReadU8('max_players');
 reader.ReadU8('opt_players');
 reader.ReadBool('press_any_key')
@@ -24,11 +24,11 @@ reader.ReadU32('voice_falloff')
 reader.ReadFP32('voice_volume')
 
 // TEXT DATA
-reader.ReadHintStr('text', 'chose')
-reader.ReadHintStr('title', 'chose')
+reader.ReadHintStr('text', 'choose')
+reader.ReadHintStr('title', 'choose')
 reader.ReadU32('text_delay')
-reader.ReadVec4('text_color', 'color, vec4')
-reader.ReadFP32('text_indient')
+reader.ReadVec4('text_rect')
+reader.ReadFP32('title_indent')
 
 // TEXTURE DATA
 reader.ReadHintStr('progress_map', 'texture, str_shared')
@@ -36,12 +36,12 @@ reader.ReadU32('progress_duration')
 
 // LABELS DATA
 var labels = reader.ReadArray('labels')
-for (var i = 0; i < labels.More(); i++) {
-    var rect = labels.ReadSection(RecStr("_rec", i, 4), false)
+for (var i = 0; labels.More(); i++) {
+    var rect = labels.ReadSection(RecStr("rec_", i, 4), false)
     rect.ReadFP32('font_size')
     rect.ReadU32('line_spacing')
-    rect.ReadVec4('color', 'color, vec4')
-    rect.ReadHintStr('text', 'chose')
+    rect.ReadVec4('color', 'color, vec4f')
+    rect.ReadHintStr('text', 'choose')
     rect.ReadVec4('rect')
     rect.ReadBool('fade_in')
 }
